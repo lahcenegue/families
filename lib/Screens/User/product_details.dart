8de520c62bcae595716d9eafa703b9180516994f
lahcenegue <lahcene90.lg@gmail.com>
2024-04-06 07:22:44 +0100

    update products details

--- lib/Screens/User/product_details.dart
+++ lib/Screens/User/product_details.dart
@@ -43,127 +43,160 @@ class ProductDetails extends StatelessWidget {
                         ),
                         child: CustomScrollView(
                           slivers: [
-                            SliverToBoxAdapter(
-                              child: SizedBox(height: appSettings.width * 0.2),
+                            SliverFillRemaining(
+                              hasScrollBody: false,
+                              child: Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      child: SizedBox(
+                                          height: appSettings.width * 0.28),
+                                    ),
+                                    Text(
+                                      'Fried Shrimp',
+                                      textAlign: TextAlign.center,
+                                      style: AppStyles.styleBold(context, 20),
+                                    ),
+                                    Text(
+                                      'This is my kind of breakfast egg sandwich and it takes under  5 minutes to make',
+                                      textAlign: TextAlign.center,
+                                      style: AppStyles.styleMedium(context, 13)
+                                          .copyWith(
+                                        color: AppColors.greyTextColors,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 15),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.star_border_rounded,
+                                          color: AppColors.greyTextColors,
+                                        ),
+                                        const SizedBox(width: 5),
+                                        Text(
+                                          '4.8(163)',
+                                          style:
+                                              AppStyles.styleBold(context, 12)
+                                                  .copyWith(
+                                            color: AppColors.greyTextColors,
+                                          ),
+                                        ),
+                                        const SizedBox(width: 20),
+                                        Icon(
+                                          Icons.timer_outlined,
+                                          color: AppColors.greyTextColors,
+                                        ),
+                                        const SizedBox(width: 5),
+                                        Text(
+                                          '20 min',
+                                          style:
+                                              AppStyles.styleBold(context, 12)
+                                                  .copyWith(
+                                            color: AppColors.greyTextColors,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 40),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Ingrigients',
+                                          style:
+                                              AppStyles.styleBold(context, 15),
+                                        ),
+                                        Text(
+                                          '7 items',
+                                          style:
+                                              AppStyles.styleMedium(context, 13)
+                                                  .copyWith(
+                                            color: AppColors.greyTextColors,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    Container(
+                                      width: appSettings.width,
+                                      height: 140,
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 10),
+                                      // color: Colors.white,
+                                      child: ListView.separated(
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount: 7,
+                                        separatorBuilder: (context, index) =>
+                                            const SizedBox(width: 10),
+                                        itemBuilder: (context, index) {
+                                          return const IngridientsBox(
+                                            image: 'assets/images/brocoli.png',
+                                            title: 'Brocoli',
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                    const SizedBox(height: 40),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          child: Image.asset(
+                                              'assets/images/41.png'),
+                                        ),
+                                        const SizedBox(width: 10),
+                                        Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'Product family ',
+                                              style: AppStyles.styleMedium(
+                                                  context, 20),
+                                            ),
+                                            const Icon(
+                                              Icons.star,
+                                              color: Color(0xffFDB022),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    const SizedBox(height: 40),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          'Size',
+                                          style: AppStyles.styleRegular(
+                                              context, 15),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 40),
+                                  ],
+                                ),
+                              ),
                             ),
-                            // Text(
-                            //   'Fried Shrimp',
-                            //   textAlign: TextAlign.center,
-                            //   style: AppStyles.styleBold(context, 20),
-                            // ),
-                            // Text(
-                            //   'This is my kind of breakfast egg sandwich and it takes under  5 minutes to make',
-                            //   textAlign: TextAlign.center,
-                            //   style:
-                            //       AppStyles.styleMedium(context, 13).copyWith(
-                            //     color: AppColors.greyTextColors,
-                            //   ),
-                            // ),
-                            // const SizedBox(height: 15),
-                            // Row(
-                            //   mainAxisAlignment: MainAxisAlignment.center,
-                            //   children: [
-                            //     Icon(
-                            //       Icons.star_border_rounded,
-                            //       color: AppColors.greyTextColors,
-                            //     ),
-                            //     const SizedBox(width: 5),
-                            //     Text(
-                            //       '4.8(163)',
-                            //       style:
-                            //           AppStyles.styleBold(context, 12).copyWith(
-                            //         color: AppColors.greyTextColors,
-                            //       ),
-                            //     ),
-                            //     const SizedBox(width: 20),
-                            //     Icon(
-                            //       Icons.timer_outlined,
-                            //       color: AppColors.greyTextColors,
-                            //     ),
-                            //     const SizedBox(width: 5),
-                            //     Text(
-                            //       '20 min',
-                            //       style:
-                            //           AppStyles.styleBold(context, 12).copyWith(
-                            //         color: AppColors.greyTextColors,
-                            //       ),
-                            //     ),
-                            //   ],
-                            // ),
-                            // const SizedBox(height: 40),
-                            // Row(
-                            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            //   children: [
-                            //     Text(
-                            //       'Ingrigients',
-                            //       style: AppStyles.styleBold(context, 15),
-                            //     ),
-                            //     Text(
-                            //       '7 items',
-                            //       style: AppStyles.styleMedium(context, 13)
-                            //           .copyWith(
-                            //         color: AppColors.greyTextColors,
-                            //       ),
-                            //     )
-                            //   ],
-                            // ),
-                            // Container(
-                            //   width: appSettings.width,
-                            //   height: 140,
-                            //   padding: const EdgeInsets.symmetric(vertical: 10),
-                            //   // color: Colors.white,
-                            //   child: ListView.separated(
-                            //     scrollDirection: Axis.horizontal,
-                            //     itemCount: 7,
-                            //     separatorBuilder: (context, index) =>
-                            //         const SizedBox(width: 10),
-                            //     itemBuilder: (context, index) {
-                            //       return const IngridientsBox(
-                            //         image: 'assets/images/brocoli.png',
-                            //         title: 'Brocoli',
-                            //       );
-                            //     },
-                            //   ),
-                            // ),
-                            // const SizedBox(height: 40),
-                            // Row(
-                            //   mainAxisAlignment: MainAxisAlignment.start,
-                            //   children: [
-                            //     SizedBox(
-                            //       child: Image.asset('assets/images/41.png'),
-                            //     ),
-                            //     const SizedBox(width: 10),
-                            //     Column(
-                            //       mainAxisAlignment:
-                            //           MainAxisAlignment.spaceAround,
-                            //       crossAxisAlignment: CrossAxisAlignment.start,
-                            //       children: [
-                            //         Text(
-                            //           'Product family ',
-                            //           style: AppStyles.styleMedium(context, 20),
-                            //         ),
-                            //         const Icon(
-                            //           Icons.star,
-                            //           color: Color(0xffFDB022),
-                            //         )
-                            //       ],
-                            //     )
-                            //   ],
-                            // ),
-                            // const SizedBox(height: 40),
-                            // Row(
-                            //   children: [
-                            //     Text(
-                            //       'Size',
-                            //       style: AppStyles.styleRegular(context, 15),
-                            //     ),
-                            //   ],
-                            // ),
-                            // Container(
-                            //   width: appSettings.width,
-                            //   height: appSettings.height * 0.2,
-                            //   color: const Color(0xFF161616),
-                            // ),
+                            SliverToBoxAdapter(
+                              child: Container(
+                                padding: const EdgeInsets.all(12),
+                                height: appSettings.height * 0.3,
+                                width: appSettings.width,
+                                decoration: const BoxDecoration(
+                                  color: Color(0xFF161616),
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(30),
+                                    topRight: Radius.circular(30),
+                                  ),
+                                ),
+                              ),
+                            )
                           ],
                         ),
                       ),
@@ -172,7 +205,7 @@ class ProductDetails extends StatelessWidget {
                   Align(
                     alignment: Alignment.topCenter,
                     child: Container(
-                      padding: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(12),
                       width: appSettings.width * 0.6,
                       //color: Colors.white,
                       child: AspectRatio(
